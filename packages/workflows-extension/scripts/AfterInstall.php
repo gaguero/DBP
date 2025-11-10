4a9639060289d03905f92c57ca648c7811ea4138
Your Name <your.email@example.com>
2025-11-10 13:58:22 -0500

    chore(workflows): add tab auto-registration in extension scripts

--- packages/workflows-extension/scripts/AfterInstall.php
+++ packages/workflows-extension/scripts/AfterInstall.php
@@ -1,1 +1,26 @@
-<?php\n\nuse Espo\\Core\\Container;\n\nreturn function (Container ): void {\n    ->get('dataCacheManager')->clearAll();\n    ->get('rebuildManager')->rebuild(false);\n};\n
+<?php
+
+use Espo\Core\Container;
+use Espo\Core\Utils\Config\ConfigWriter;
+
+return function (Container $container): void {
+    $config = $container->get('config');
+    $configWriter = $container->get('injectableFactory')->create(ConfigWriter::class);
+
+    $tabList = $config->get('tabList') ?? [];
+    if (!in_array('Workflow', $tabList, true)) {
+        $tabList[] = 'Workflow';
+        $configWriter->set('tabList', $tabList);
+    }
+
+    $quickCreateList = $config->get('quickCreateList') ?? [];
+    if (!in_array('Workflow', $quickCreateList, true)) {
+        $quickCreateList[] = 'Workflow';
+        $configWriter->set('quickCreateList', $quickCreateList);
+    }
+
+    $configWriter->save();
+
+    $container->get('dataCacheManager')->clearAll();
+    $container->get('rebuildManager')->rebuild(false);
+};
--- packages/workflows-extension/scripts/AfterUninstall.php
+++ packages/workflows-extension/scripts/AfterUninstall.php
@@ -1,1 +1,26 @@
-<?php\n\nuse Espo\\Core\\Container;\n\nreturn function (Container ): void {\n    ->get('dataCacheManager')->clearAll();\n    ->get('rebuildManager')->rebuild(false);\n};\n
+<?php
+
+use Espo\Core\Container;
+use Espo\Core\Utils\Config\ConfigWriter;
+
+return function (Container $container): void {
+    $config = $container->get('config');
+    $configWriter = $container->get('injectableFactory')->create(ConfigWriter::class);
+
+    $tabList = array_values(array_filter(
+        $config->get('tabList') ?? [],
+        fn (string $item): bool => $item !== 'Workflow'
+    ));
+    $configWriter->set('tabList', $tabList);
+
+    $quickCreateList = array_values(array_filter(
+        $config->get('quickCreateList') ?? [],
+        fn (string $item): bool => $item !== 'Workflow'
+    ));
+    $configWriter->set('quickCreateList', $quickCreateList);
+
+    $configWriter->save();
+
+    $container->get('dataCacheManager')->clearAll();
+    $container->get('rebuildManager')->rebuild(false);
+};
